150196df728362355686f3cf19be0b0c7357ef7c
Hilary Stohs-Krause <hilarysk@gmail.com>
2015-04-12 11:39:36 -0500

    initializes

---
+++ notes.rb
@@ -0,0 +1,8 @@
+http://svn.code.sf.net/p/cmusphinx/code/trunk/cmudict/
+- to convert to IPA http://en.wikipedia.org/wiki/Arpabet
+- same as above http://clair.si.umich.edu/~radev/NLP/notes/abney/pdffigures/fig04.02.pdf
+
+Need to change CMU symbols to IPA
+then make dictionary into database form
+-using scraper? not sure best how to do that -- surely there's a way though. 
+Maybe a program that converts word document to database?
